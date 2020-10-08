13d5758b0ff4e3fcc7f5209a4164833bb37e03e6
Agustín Mafud <agustin.mafud@globant.com>
2020-10-08 15:19:08 -0300

    Redux added with empty store config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducer from './store/reducers';
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   // eslint-disable-next-line no-undef
   document.getElementById('root'),
